53df7458f15cded2e389e9df848de7743bc4a10d
Kane <kanetan4@gmail.com>
2023-01-16 14:54:20 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,15 +4,17 @@ import cvzone
 import numpy as np
  
 # Video feed
-cap = cv2.VideoCapture('./videos/CPLatestVid.mp4')
+feed = cv2.VideoCapture('./videos/CPLatestVid.mp4')
+
+#setting size of the boxes 
+width, height = 40,17
  
 with open('CarParkPos2', 'rb') as f:
     posList = pickle.load(f)
- 
-width, height = 40,17
+
 
 def checkParkingSpace(imgPro):
-    spaceCounter = 0
+    emptySpace = 0
  
     for pos in posList:
         x, y = pos
@@ -21,36 +23,39 @@ def checkParkingSpace(imgPro):
         # cv2.imshow(str(x * y), imgCrop)
         count = cv2.countNonZero(imgCrop)
  
- 
+        #detection criteria of empty lots, 200 pixels
         if count < 200:
             color = (0, 255, 0)
             thickness = 1
-            spaceCounter += 1
+            emptySpace += 1
         else:
             color = (0, 0, 255)
             thickness = 1
  
+        #create lot detection boxes
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        
+        #count number of pixels within the box
         cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=0.6,
                            thickness=1, offset=0, colorR=color)
- 
-    cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (150, 50), scale=2,
+    
+    #display total number of empty lots
+    cvzone.putTextRect(img, f'Free: {emptySpace}/{len(posList)}', (150, 50), scale=2,
                            thickness=3, offset=20, colorR=(0,200,0))
+
+#main function body to run live vidoe feed
 while True:
- 
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    success, img = cap.read()
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if feed.get(cv2.CAP_PROP_POS_FRAMES) == feed.get(cv2.CAP_PROP_FRAME_COUNT):
+        feed.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    success, img = feed.read()
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                          cv2.THRESH_BINARY_INV, 25, 16)
-    imgMedian = cv2.medianBlur(imgThreshold, 5)
     kernel = np.ones((3, 3), np.uint8)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
     imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
  
     checkParkingSpace(imgDilate)
     cv2.imshow("Image", img)
-    #cv2.imshow("ImageBlur", imgBlur)
-    #cv2.imshow("ImageThres", imgMedian)
     cv2.waitKey(10)
